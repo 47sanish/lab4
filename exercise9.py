e0f49fee96bf6069e4013c6ab57d8077fafdc78e
Shanish <chaudharysanish54@gmail.com>
2021-03-23 09:16:44 +0545

    exercise 9

---
+++ exercise9.py
@@ -0,0 +1,8 @@
+#write a python program to convert seconds to day,hour,minutes and second.
+sec=int(input("enter the second you want to convert into day,hour minute"))
+day=sec/(60*60*24)
+hour=sec/(60*60)
+minute=sec/(60)
+print(f"the day is {day}")
+print(f"the hour is {hour}")
+print(f"the minute is {minute}")
